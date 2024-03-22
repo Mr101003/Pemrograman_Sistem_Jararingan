afb3477ab2b82616724d17643caa6712e54f17e6
Maxzcv <59911607+Mr101003@users.noreply.github.com>
2024-03-22 15:10:45 +0700

    praktikum 1

--- memmory usage  bash /memmory.py
+++ memmory usage  bash /memmory.py
@@ -1,12 +1,31 @@
 import os
-instruction = "ps aux"
+import xlsxwriter
+
+instruction = "ps -eo pid,rss,cmd --sort -rss"
 cmd = os.popen(instruction)
 result = cmd.readlines()
 data_clean = result[3:]
+task = []
 for line in data_clean:
     data = line.split()
-    user = data[0]
-    memmory = data[2]
-    command = data[10].split("/")
+#     user = data[0]
+    memmory = data[1]
+    command = data[2].split("/")
     program = command[-1]
-    print(f"{program} => {memmory}")
+    
+    try:
+        task.append( (float(memmory),program))
+    except:
+        pass
+    
+data_memmory = []
+data_task = []
+
+for baris in task[:5]:
+    print(f"{baris[1]} => {baris[0]}")
+    data_memmory.append(baris[0])
+    data_task.append(baris[1])
+    
+    # print(f"{program} => {memmory}")
+# print(data_clean)
+
